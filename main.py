8db1108f0d380de56611353b637b221c5793abcf
Gabriel-2511 <165502792+Gabriel-2511@users.noreply.github.com>
2025-06-22 20:16:30 -0400

    Corrección de algunos errores

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -12,10 +12,10 @@ async def on_ready():
 
 @bot.command()
 async def materiales(ctx):
-    print("Pueden reciclarse metales, papeles, plásticos y vidrios en sus respectivos contenedores. También pueden ser reciclados algunos equipos electrónicos en puntos limpios.")
+    await ctx.send("Pueden reciclarse metales, papeles, plásticos y vidrios en sus respectivos contenedores. También pueden ser reciclados algunos equipos electrónicos en puntos limpios.")
 
 @bot.command()
 async def contenedores(ctx):
-    print("En el contenedor azul van papeles y cartones, en el café van residuos orgánicos, en el gris deben ir desechos no reciclables, en el verde se echa el vidrio, y en el amarillo van plásticos, latas y envases de cartón para líquidos (bricks).")
+    await ctx.send("En el contenedor azul van papeles y cartones, en el café van residuos orgánicos, en el gris deben ir desechos no reciclables, en el verde se echa el vidrio, y en el amarillo van plásticos, latas y envases de cartón para líquidos (bricks).")
 
 bot.run("Token")
